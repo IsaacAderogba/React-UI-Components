da1b12565de767a163249b451a992718c49ccdfd
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-05-27 14:08:06 +0100

    Add footer icons to and append to app

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-container">
+        <div>
+          <i className="material-icons">chat_bubble_outline</i>
+          <span></span>
+        </div>
+        <div>
+          <i className="material-icons">cached</i>
+          <span></span>
+        </div>
+        <div>
+          <i className="material-icons">favorite_border</i>
+          <span>3</span>
+        </div>
+        <div>
+          <i className="material-icons">mail_outline</i>
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
