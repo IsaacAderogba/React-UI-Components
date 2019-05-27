663bd4a741ba04f59f33fb056ef605649f5b6ee5
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-05-27 12:58:24 +0100

    Construct header componenent and attach to App component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+        <div className="header-title">
+            <h3>Lambda School</h3>
+            <span>@LambdaSchool</span>
+            <span>26 Jan</span>
+        </div>
+    );
+}
+
+export default HeaderTitle;
