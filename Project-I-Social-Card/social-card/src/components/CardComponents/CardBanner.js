fc21e41cb6222201b70282b9c301361f94f2ce88
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-05-27 13:29:13 +0100

    Add redirect to react website once the card is clicekd

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Card.css";
 
 function CardBanner() {
-  return <div className="card-image-background" />;
+  return (
+    <a href="https://www.reactjs.org">
+      <div className="card-image-background" />
+    </a>
+  );
 }
 
 export default CardBanner;
