b4e8b0882702319f189571869e03a20ba129057d
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-05-27 13:08:53 +0100

    Construct card container component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+function CardContainer() {
+  return (
+    <div className="card-wrapper">
+      <div className="card-container">Hi</div>
+    </div>
+  );
+}
+
+export default CardContainer;
